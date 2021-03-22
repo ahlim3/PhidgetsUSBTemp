e6b14a5843979dd9bf497a43f4c8d7b6b22d8be2
Jeff Terry <terryj@iit.edu>
2021-03-22 16:53:57 -0500

    Initial Commit

---
+++ PhidgetsUSBTemp/PhidgetsUSBTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhidgetsUSBTempApp.swift
+//  PhidgetsUSBTemp
+//
+//  Created by Jeff Terry on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct PhidgetsUSBTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
